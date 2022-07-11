c61a911aae46f517024e1dbb3e5d158cfb883527
EbicHecker <ebicdev@pm.me>
2022-07-11 23:26:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Builder;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
+
+var module = ModuleDefinition.FromFile(args[0]);
+foreach (var instruction in (module.ManagedEntrypointMethod?.CilMethodBody ?? throw new NotSupportedException()).Instructions)
+{
+    if (instruction.Operand is CilInstructionLabel
+        {
+            Instruction:
+            { OpCode.Code: CilCode.Call, Operand: MethodDefinition { Signature: { ReturnsValue: true } signature } method }
+        } && signature.ReturnType == module.CorLibTypeFactory.Boolean && method.CilMethodBody?.LocalVariables.Count is 1)
+    {
+        method.CilMethodBody = new CilMethodBody(method)
+        {
+            Instructions =
+            {
+                new CilInstruction(CilOpCodes.Ldc_I4_1),
+                new CilInstruction(CilOpCodes.Ret)
+            }
+        };
+        
+        Console.WriteLine($"removed: {method}");
+    }
+}
+
+module.Write(args[0].Insert(args[0].Length - 4, "-no_trial"), new ManagedPEImageBuilder(MetadataBuilderFlags.PreserveAll));
